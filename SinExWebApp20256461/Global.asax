7944a765cf2ade5400b9ad048ce9a1d0867da22c
xduac <xduac@CSL4WK10>
2017-02-07 12:57:45 +0800

    Add project files.

---
+++ SinExWebApp20256461/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SinExWebApp20256461.MvcApplication" Language="C#" %>
---
+++ SinExWebApp20256461/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SinExWebApp20256461.Startup))]
+namespace SinExWebApp20256461
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
