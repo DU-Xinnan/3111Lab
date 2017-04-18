67b94511ce02fd0b7bed09d7979e38dff1edefa4
DU Xinnan <xduac@WIN2K.CS.UST.HK>
2017-04-18 09:38:26 +0800

    change model

--- SinExWebApp20256461/Models/Shipment.cs
+++ SinExWebApp20256461/Models/Shipment.cs
@@ -16,12 +16,20 @@ namespace SinExWebApp20256461.Models
         public virtual string ServiceType { get; set; }
         public virtual DateTime ShippedDate { get; set; }
         public virtual DateTime DeliveredDate { get; set; }
-        public virtual string RecipientName { get; set; }
+        public virtual string RecipientName { get; set; } // what is dis ???
         public virtual int NumberOfPackages { get; set; }
-        public virtual string Origin { get; set; }
-        public virtual string Destination { get; set; }
+        public virtual string Origin { get; set; } // why str ???
+        public virtual string Destination { get; set; } // why str ???
         public virtual string Status { get; set; }
         public virtual int ShippingAccountId { get; set; }
         public virtual ShippingAccount ShippingAccount { get; set; }
+        public virtual int RecipientID { get; set; }
+        public virtual Recipient Recipient { get; set; }
+        //417
+        public virtual bool IfSendEmail { get; set; }
+        public virtual ICollection<Invoice> Invoices { get; set; }
+        public virtual ICollection<Package> Packages { get; set; }
+        public virtual int PickupID { get; set; }
+        public virtual Pickup Pickup { get; set; }
     }
 }
